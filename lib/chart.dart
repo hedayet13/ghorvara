bbc2038be2932fb5065cdd461cf536197d12e0dc
Hedayet Islam <islamhedayet67@gmail.com>
2021-02-09 19:56:37 +0600

    Current bill selection added

--- lib/chart.dart
+++ lib/chart.dart
@@ -12,26 +12,44 @@ class _chartState extends State<chart> {
   bool _validateCurrent = false;
   bool _validateGas = false;
   bool _validatewater = false;
+
   bool value1 = false;
   bool value2 = false;
+  bool currentValue1 = false;
+  bool currentValue2 = false;
 
   var rent = 0, current = 0, gas = 0, water = 0;
   var stoveNum = 0;
   double sum = 0;
-  double totalCurrent = 0;
+  double totalCurrent = 0, currentUnit = 0;
 
-  final rentController = TextEditingController();
-  final currentBillController = TextEditingController();
+  final rentController = TextEditingController(text: "0");
+  final currentBillController = TextEditingController(text: "0");
   // final gasBillController = TextEditingController();
-  final waterBillController = TextEditingController();
+  final waterBillController = TextEditingController(text: "0");
 
   void addition() {
     setState(() {
       rent = int.parse(rentController.text);
       current = int.parse(currentBillController.text);
-      totalCurrent = current * 5.50;
+      totalCurrent = current * currentUnit;
       // gas = int.parse(gasBillController.text);
       water = int.parse(waterBillController.text);
+
+      if ((currentValue1 == false && currentValue2 == false) ||
+          (value1 == false && value2 == false)) {
+        return showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                backgroundColor: Colors.red,
+                content: Text(
+                  '''Please select the check the box of current bill "In Unit" or "In TaKa" and Gas bill "Single Stove" or "Double Stove"''',
+                  style: TextStyle(color: Colors.white),
+                ),
+              );
+            });
+      }
 
       sum = rent + totalCurrent + gas + water;
     });
@@ -74,15 +92,16 @@ class _chartState extends State<chart> {
                   keyboardType: TextInputType.number,
                 ),
                 Padding(padding: EdgeInsets.only(top: 10)),
-                TextFormField(
-                  controller: currentBillController,
-                  decoration: InputDecoration(
-                      labelText: "Current bill (in unit)",
-                      errorText: _validateCurrent ? "Can't be Empty" : null,
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(MdiIcons.flash)),
-                  keyboardType: TextInputType.number,
-                ),
+                Current(),
+                // TextFormField(
+                //   controller: currentBillController,
+                //   decoration: InputDecoration(
+                //       labelText: "Current bill (in unit)",
+                //       errorText: _validateCurrent ? "Can't be Empty" : null,
+                //       border: OutlineInputBorder(),
+                //       prefixIcon: Icon(MdiIcons.flash)),
+                //   keyboardType: TextInputType.number,
+                // ),
                 Padding(padding: EdgeInsets.only(top: 10)),
                 // TextFormField(
                 //   controller: gasBillController,
@@ -136,8 +155,8 @@ class _chartState extends State<chart> {
                           context: context,
                           builder: (context) {
                             return AlertDialog(
-                              content: Text('''Number of stove = $stoveNum stove
-Total Rent is = $sum taka'''),
+                              content: Text(
+                                  '''Number of stove = $stoveNum stove \nTotal Rent is = $sum taka '''),
                             );
                           });
                     },
@@ -161,6 +180,59 @@ Total Rent is = $sum taka'''),
     );
   }
 
+  Current() {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.black38),
+          borderRadius: BorderRadius.all(Radius.circular(2))),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(padding: EdgeInsets.only(left: 10)),
+              Text("Current Bill"),
+              Checkbox(
+                  value: currentValue1,
+                  onChanged: (currentValue1) {
+                    setState(() {
+                      this.currentValue1 = currentValue1;
+                      if (currentValue1 == true) {
+                        currentUnit = 5.50;
+                        currentValue2 = false;
+                        // stoveNum = 1;
+                      }
+                    });
+                  }),
+              Text("In units"),
+              Checkbox(
+                  value: currentValue2,
+                  onChanged: (currentValue2) {
+                    setState(() {
+                      this.currentValue2 = currentValue2;
+                      if (currentValue2 = true) {
+                        currentUnit = 1;
+                        currentValue1 = false;
+                        // print("Currentunit = 0");
+                        // stoveNum = 1;
+                      }
+                    });
+                  }),
+              Text("In Taka"),
+            ],
+          ),
+          TextFormField(
+            controller: currentBillController,
+            decoration: InputDecoration(
+                labelText: "Current bill (in unit)",
+                errorText: _validateCurrent ? "Can't be Empty" : null,
+                prefixIcon: Icon(MdiIcons.flash)),
+            keyboardType: TextInputType.number,
+          ),
+        ],
+      ),
+    );
+  }
+
   // ignore: non_constant_identifier_names
   Gas() {
     return Container(
@@ -175,7 +247,7 @@ Total Rent is = $sum taka'''),
                 setState(() {
                   this.value1 = value1;
                   if (value1 == true) {
-                    gas = 500;
+                    gas = 925;
                     value2 = false;
                     stoveNum = 1;
                   } else {
@@ -211,6 +283,5 @@ Total Rent is = $sum taka'''),
 }
 
 //ToDo current bill value per unit taka or full value
-//todo Gasbill for one stove or two stove or normal value
 //todo save data monthly
 //todo Add calender for save data
