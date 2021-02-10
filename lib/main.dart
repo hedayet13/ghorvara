a646002051d9f242aca12d7593f0ebbe6d94f06c
Hedayet Islam <islamhedayet67@gmail.com>
2021-02-10 15:51:29 +0600

    LandscapeMode Off

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter/services.dart';
 import 'package:ghorvara/chart.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MyApp());
 }
 
@@ -36,64 +39,66 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Colors.black,
       body: Padding(
         padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-        child: Column(
-          children: [
-            Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(padding: EdgeInsets.only(top: 250)),
+                    Text(
+                      "Welcome to",
+                      style: TextStyle(
+                          fontSize: 140,
+                          fontFamily: 'brownies_cake',
+                          color: Colors.greenAccent),
+                      textAlign: TextAlign.left,
+                    ),
+                    Text(
+                      "GHORVARA",
+                      style: TextStyle(
+                          fontSize: 90,
+                          fontFamily: 'brownies_cake',
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.start,
+                    ),
+                  ],
+                ),
+              ),
+              Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Padding(padding: EdgeInsets.only(top: 250)),
-                  Text(
-                    "Welcome to",
-                    style: TextStyle(
-                        fontSize: 140,
-                        fontFamily: 'brownies_cake',
-                        color: Colors.greenAccent),
-                    textAlign: TextAlign.left,
-                  ),
-                  Text(
-                    "GHORVARA",
-                    style: TextStyle(
-                        fontSize: 90,
-                        fontFamily: 'brownies_cake',
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.start,
-                  ),
-                ],
-              ),
-            ),
-            Center(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(padding: EdgeInsets.only(top: 250)),
-                // ignore: deprecated_member_use
-                SizedBox(
-                  height: 60,
-                  width: 300,
                   // ignore: deprecated_member_use
-                  child: RaisedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => chart()));
-                    },
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Text(
-                      "Lets put your data",
-                      style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.normal,
-                          fontFamily: 'brownies_cake',
-                          color: Colors.black),
+                  SizedBox(
+                    height: 60,
+                    width: 300,
+                    // ignore: deprecated_member_use
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => chart()));
+                      },
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Text(
+                        "Lets put your data",
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.normal,
+                            fontFamily: 'brownies_cake',
+                            color: Colors.black),
+                      ),
+                      color: Colors.greenAccent,
                     ),
-                    color: Colors.greenAccent,
-                  ),
-                )
-              ],
-            )),
-          ],
+                  )
+                ],
+              )),
+            ],
+          ),
         ),
       ),
     );
